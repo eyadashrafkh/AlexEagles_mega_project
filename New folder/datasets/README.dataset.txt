0233e72baec65cc463d663289587be3ee27a739f
Iyad ashraf <eyad.ashraf941@gmail.com>
2023-09-28 18:52:25 +0300

    Uploading task 2

---
+++ New folder/datasets/README.dataset.txt
@@ -0,0 +1,6 @@
+# suas final > dataset
+https://universe.roboflow.com/vidhi6khosla-gmail-com/suas-final
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ New folder/image_processing.py
@@ -0,0 +1,86 @@
+import os
+import cv2
+import numpy as np
+from sklearn.cluster import KMeans
+
+# Function to resize the images
+def resize_image(image, size):
+    resized_image = cv2.resize(image, size)
+    return resized_image
+
+# Function to separate colors using KMeans clustering
+def separate_colors(image, num_clusters):
+    # Reshape the image into a 2D array of pixels
+    pixels = image.reshape(-1, 3)
+    
+    # Apply KMeans clustering to find the dominant colors
+    kmeans = KMeans(n_clusters=num_clusters)
+    kmeans.fit(pixels)
+    
+    # Get the cluster labels for each pixel
+    labels = kmeans.labels_
+    
+    # Reshape the labels back to the original image shape
+    segmented_image = labels.reshape(image.shape[:2])
+    
+    # Get the background cluster label
+    background_label = np.argmax(np.bincount(segmented_image.flatten()))
+    
+    # Create a mask to filter out the background pixels
+    mask = np.where(segmented_image != background_label, 255, 0).astype(np.uint8)
+    
+    # Apply the mask to the original image
+    masked_image = cv2.bitwise_and(image, image, mask=mask)
+    
+    return masked_image, mask
+
+# Directory containing the images
+image_directory = 'New folder\Inference_data'
+
+# Size to resize the images
+resize_size = (640, 640)
+
+# Number of clusters for KMeans clustering
+num_clusters = 3
+
+# Read images from the directory, resize, and remove background
+for filename in os.listdir(image_directory):
+    if filename.endswith('.jpg') or filename.endswith('.png'):
+        image_path = os.path.join(image_directory, filename)
+        
+        # Read the image
+        image = cv2.imread(image_path)
+        
+        # Resize the image
+        resized_image = resize_image(image, resize_size)
+        
+        # Remove background color using KMeans clustering
+        masked_image, mask = separate_colors(resized_image, num_clusters)
+        
+        # Convert the masked image to grayscale
+        grayscale_image = cv2.cvtColor(masked_image, cv2.COLOR_BGR2GRAY)
+        
+        # Apply thresholding to obtain binary image
+        _, binary_image = cv2.threshold(grayscale_image, 1, 255, cv2.THRESH_BINARY)
+        
+        # Find contours in the binary image
+        contours, _ = cv2.findContours(binary_image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        
+        # Crop contour areas
+        cropped_images = []
+        for contour in contours:
+            # Create a bounding rectangle around the contour
+            x, y, w, h = cv2.boundingRect(contour)
+            
+            # Crop the image using the bounding rectangle coordinates
+            cropped_image = resized_image[y:y+h, x:x+w]
+            
+            # Add the cropped image to the list
+            cropped_images.append(cropped_image)
+        
+        # Display the cropped contour areas
+        for i, cropped_image in enumerate(cropped_images):
+            cv2.imshow(f'Cropped Image {i+1}', cropped_image)
+        
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
